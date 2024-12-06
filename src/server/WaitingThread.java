275aaccb542e303b632738a5f4ce5a0e14a48836
david-parkk <ji0513ji@naver.com>
2024-12-06 19:11:04 +0900

    fix: 버그 수정

--- src/server/WaitingThread.java
+++ src/server/WaitingThread.java
@@ -3,7 +3,7 @@ package server;
 public class WaitingThread extends Thread {
 
     public static GameThread gameThread;
-    public static int gamePlayers_num = 2;
+    public static int gamePlayers_num = 4;
     //여기 4로 수정
     public WaitingThread() {
         System.out.println("server.WaitingThread constructor");
